8788d9f13b4589760c764ccaf87e8926e809ab05
Gombka25@gmail.com <l.sypniewski@gmail.com>
2017-07-07 10:34:35 +0200

    v2

--- SeleniumIntro.py
+++ SeleniumIntro.py
@@ -1,9 +1,10 @@
 from selenium.webdriver import Edge
 from selenium.webdriver import Firefox
 #edge = Edge(executable_path=r"C:\Users\U112918\Downloads\MicrosoftWebDriver.exe")
-Driver = Edge()
+driver = Edge()
 
-Driver.get("http://www.allegro.pl")
-search_bar = Driver.find_element_by_id("main-search-text")
-search_bar.send_keys("Reanult Laguna")
-search_bar.click()
+driver.get("http://www.wykop.pl")
+element = driver.find_element_by_partial_link_text("kompresja") Lapie tylko tytul linka, nie href#<a title="1K17 Kompresja - radziecki pojazd uzbrojony w lasery" href="https://www.wykop.pl/link/3816569/1k17-kompresja-radziecki-pojazd-uzbrojony-w-lasery/">1K17 Kompresja - radziecki pojazd uzbrojony w lasery</a>
+element.click()
+
+assert "Radziecki" in driver.title
